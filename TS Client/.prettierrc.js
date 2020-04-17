8c6aaf5212119954dc12850561003f60770fb337
Onurcan Camci <onurcamci@hotmail.com>
2020-04-18 02:58:20 +0300

    feat: ts client started

---
+++ TS Client/.prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    semi: true,
+    trailingComma: "all",
+    printWidth: 80,
+    tabWidth: 4,
+    useTabs: false,
+};
---
+++ TS Client/src/adaptor.ts
@@ -0,0 +1,9 @@
+export enum Sizes {
+    u8 = 1,
+    u16 = 2,
+    u32 = 4,
+    u64 = 8,
+    usize = 8,
+}
+
+export class Serializer {}
